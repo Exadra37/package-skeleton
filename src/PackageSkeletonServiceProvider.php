3b162e6f3a1aae7aae5d46b7740728c1c42151f4
D'Arcy Rush <drush@nn4m.com>
2016-04-20 15:24:29 +0100

    Add factory loading from within package

--- src/PackageSkeletonServiceProvider.php
+++ src/PackageSkeletonServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Exadra37\PackageSkeleton;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
 
 class PackageSkeletonServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,33 @@ class PackageSkeletonServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->updateModelFactoriesPath();
+        $this->bindInterfaces();
+    }
+
+    /**
+     * Change the model factories path to point within the package
+     *
+     * @return void
+     */
+    protected function updateModelFactoriesPath()
+    {
+        if ($this->app->environment() === 'testing') {
+            $this->app->singleton('Illuminate\Database\Eloquent\Factory', function () {
+                $faker = \Faker\Factory::create();
+
+                return Factory::construct($faker, __DIR__ . '/../setup/factories/');
+            });
+        }
+    }
+
+    /**
+     * Bind the interfaces to concrete implementations so Laravel's IoC knows what classes to inject
+     *
+     * @return void
+     */
+    public function bindInterfaces()
+    {
         $this->app->bind('Exadra37\PackageSkeleton\Interfaces\Controllers\BaseControllerInterface', 'Exadra37\PackageSkeleton\Controllers\BaseController');
         $this->app->bind('Exadra37\PackageSkeleton\Interfaces\Controllers\ResourceControllerInterface', 'Exadra37\PackageSkeleton\Controllers\ResourceController');
         $this->app->bind('Exadra37\PackageSkeleton\Interfaces\Exceptions\ResourceControllerExceptionInterface', 'Exadra37\PackageSkeleton\Exceptions\ResourceControllerException');
@@ -33,4 +61,5 @@ class PackageSkeletonServiceProvider extends ServiceProvider
         $this->app->bind('Exadra37\PackageSkeleton\Interfaces\Repositories\ResourceRepositoryInterface', 'Exadra37\PackageSkeleton\Repositories\ResourceRepository');
         $this->app->bind('Exadra37\PackageSkeleton\Interfaces\Services\ResourceServiceInterface', 'Exadra37\PackageSkeleton\Services\ResourceService');
     }
+
 }
